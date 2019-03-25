f21b62f803168caa1ad3e7b113d82620e3955079
Caio Cesar Gallo <caio.gallo@accurate.com.br>
2019-03-25 16:32:57 -0300

    install basic packages

--- setup.sh
+++ setup.sh
@@ -9,3 +9,7 @@ rm ~/.gitconfig
 ln -s `pwd`/zshrc ~/.zshrc
 ln -s `pwd`/terminator_config ~/.config/terminator/config
 ln -s `pwd`/gitconfig ~/.gitconfig
+
+echo installing default apps
+
+sudo pacman -S git zsh terminator firefox python
