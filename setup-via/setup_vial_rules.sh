886ab0e03495aee4144234bcffff024ded28f0f6
Caio C Gallo <caio.gallo@gmail.com>
2025-07-05 17:37:34 -0300

    update...

--- setup-via/setup_vial_rules.sh
+++ setup-via/setup_vial_rules.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-echo 'KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="0af0", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"' | sudo tee -a /etc/udev/rules.d/99-vial.rules > /dev/null
-sudo mkinitcpio -P
+sudo usermod -aG  input $USER
+sudo rm /etc/udev/rules.d/99-keychron.rules
+echo 'KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="0af0", MODE="0660", GROUP="caio", TAG+="uaccess", TAG+="udev-acl"' | sudo tee -a /etc/udev/rules.d/99-keychron.rules > /dev/null
+sudo udevadm control --reload-rules
+sudo udevadm trigger
