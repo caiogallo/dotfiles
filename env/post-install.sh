dd24844246d14a1dc307f96824bb39f799704e1d
Caio Cesar Gallo <caio.gallo@gmail.com>
2026-05-05 08:51:10 -0300

    update

---
+++ env/post-install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+omarchy-webapp-remove ChatGPT
+omarchy-webapp-remove Basecamp
+omarchy-webapp-remove Figma
+omarchy-webapp-remove Fizzy
+omarchy-webapp-remove "Google Photos" 
+omarchy-webapp-remove "Google Messages" 
+omarchy-webapp-remove "Google Contacts" 
+omarchy-webapp-remove "Google Maps" 
+omarchy-webapp-remove Google
+omarchy-webapp-remove HEY
+omarchy-webapp-remove Github
+omarchy-webapp-remove Signal
+omarchy-webapp-remove Whatsapp
+omarchy-webapp-remove 1Password
